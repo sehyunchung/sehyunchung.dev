94c9c081510b19b39d7436b8b76134fa22ffa9a6
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-05-03 22:45:08 +0900

    Add custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { make as NotFound } from '../pages/NotFound.bs'
 
 function NotFoundPage(props) {
   const { data } = props
@@ -11,8 +12,7 @@ function NotFoundPage(props) {
   return (
     <Layout location={props.location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <NotFound />
     </Layout>
   )
 }
