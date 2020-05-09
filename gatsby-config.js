de06df4e9643aa6a1f6e538b832f117505328f93
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-05-09 21:22:44 +0900

    chore: use single quote when not using string interplation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,44 @@
 module.exports = {
   siteMetadata: {
     title: '🏚',
-    author: `Sehyun Chung`,
-    description: `Yet another dev blog (in Korean).`,
-    siteUrl: `https://sehyunchung.dev/`,
+    author: 'Sehyun Chung',
+    description: 'Yet another dev blog (in Korean).',
+    siteUrl: 'https://sehyunchung.dev/',
     social: {
-      twitter: `sehyunchung`,
-      github: `sehyunchung`,
-      facebook: `sxhyxnchxng`,
+      twitter: 'sehyunchung',
+      github: 'sehyunchung',
+      facebook: 'sxhyxnchxng',
     },
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
+        name: 'assets',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
           {
@@ -55,35 +55,35 @@ module.exports = {
               extensions: ['night-owl'],
             },
           },
-					`gatsby-remark-smartypants`,
+					'gatsby-remark-smartypants',
 					'gatsby-remark-autolink-headers'
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: `UA-135916528-1`,
+        trackingId: 'UA-135916528-1',
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `sehyunchung.dev`,
-        short_name: `sehyunchung.dev`,
-        start_url: `/`,
-        background_color: `hsl(0, 0, 60%)`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/12157-anger-symbol-icon.png`,
+        name: 'sehyunchung.dev',
+        short_name: 'sehyunchung.dev',
+        start_url: '/',
+        background_color: 'hsl(0, 0, 60%)',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'content/assets/12157-anger-symbol-icon.png',
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
@@ -94,11 +94,11 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-sass`,
-    `gatsby-remark-responsive-iframe`,
-    `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-typescript`,
+    'gatsby-plugin-sass',
+    'gatsby-remark-responsive-iframe',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-typescript',
     'gatsby-plugin-robots-txt',
 		'gatsby-plugin-sitemap',
 		'gatsby-plugin-reason',
