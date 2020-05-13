d43ed645280aa2c2a6641d1ec3cfca66e09f8e47
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-05-14 00:26:20 +0900

    feat: add about page

---
+++ src/templates/About.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+export default function About() {
+  return (
+    <Layout title="🚧">
+      <h1>About</h1>
+      <p>"under construction..."</p>
+    </Layout>
+  )
+}
