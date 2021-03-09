dcc727bedfd348cf84c42a8f626f33b12dd0e8cc
Sehyun Chung <sehyun.chung@icloud.com>
2021-03-09 12:59:06 +0900

    feat: apply til page masonry layout (#148)

--- packages/gatsby-ts/src/pages/til.tsx
+++ packages/gatsby-ts/src/pages/til.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { css, jsx } from '@emotion/react'
 import snakeCase from 'lodash/snakeCase'
+import Masonry from 'react-masonry-css'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -50,99 +51,119 @@ const TilIndex: FC = () => {
       </h1>
       <ul
         css={css`
-          display: grid;
-          grid-template-columns: repeat(auto-fit, min(50ch, calc(50% - 2em)));
-          grid-template-rows: auto;
-          grid-gap: 2em;
           padding-inline-start: 0;
           list-style-type: none;
           text-align: center;
-          display: grid;
-          row-gap: 2em;
           font-size: 0.5rem;
+
+          .til-masonry-grid {
+            display: flex;
+            margin-left: -2em; /* gutter size offset */
+            width: auto;
+          }
+
+          .til-masonry-grid-column {
+            padding-left: 2em; /* gutter size */
+            background-clip: padding-box;
+          }
         `}
       >
-        {issues.map((issue) => (
-          <li
-            css={css`
-              display: flex;
-              flex-direction: column;
-            `}
-            key={issue.id}
-          >
-            <h2
+        <Masonry
+          breakpointCols={{
+            default: 6,
+            1920: 5,
+            1440: 4,
+            1024: 3,
+            854: 2,
+            640: 1,
+          }}
+          className="til-masonry-grid"
+          columnClassName="til-masonry-grid-column"
+        >
+          {issues.map((issue) => (
+            <li
               css={css`
-                font-size: 1rem;
-                padding: 0;
-                word-break: break-all;
-                text-align: left;
+                flex: 50ch;
                 display: flex;
                 flex-direction: column;
-                align-items: flex-start;
-                line-height: 1.5;
+                padding-bottom: 3em;
               `}
+              key={issue.id}
             >
-              <a href={issue.url}>{issue.title}</a>
-            </h2>
-            <ul
-              css={css`
-                list-style-type: none;
-                display: flex;
-                align-items: center;
-                padding-inline-start: 0;
-                font-size: 0.6rem;
+              <h2
+                css={css`
+                  font-size: 1rem;
+                  padding: 0;
+                  word-break: break-all;
+                  text-align: left;
+                  display: flex;
+                  flex-direction: column;
+                  align-items: flex-start;
+                  line-height: 1.5;
+                `}
+              >
+                <a href={issue.url}>{issue.title}</a>
+              </h2>
+              <ul
+                css={css`
+                  list-style-type: none;
+                  display: flex;
+                  align-items: center;
+                  padding-inline-start: 0;
+                  font-size: 0.6rem;
 
-                li:not(:last-of-type) {
-                  padding-right: 1em;
-                }
-              `}
-            >
-              {issue.labels.map((label) => (
-                <li key={label}>#{snakeCase(label)}</li>
-              ))}
-            </ul>
-            <span
-              css={css`
-                font-size: 0.6rem;
-                line-height: 3;
-                display: flex;
-                justify-content: flex-end;
-                align-items: center;
-              `}
-            >
-              {issue.updatedAt}
-            </span>
-            <div
-              css={css`
-                max-width: 100%;
-                overflow-x: auto;
-                overflow-wrap: anywhere;
-                text-align: left;
-                word-wrap: break-word;
-                word-break: break-all;
-                white-space: pre-wrap;
-                & * {
+                  li:not(:last-of-type) {
+                    padding-right: 1em;
+                  }
+                `}
+              >
+                {issue.labels.map((label) => (
+                  <li key={label}>#{snakeCase(label)}</li>
+                ))}
+              </ul>
+              <span
+                css={css`
+                  font-size: 0.6rem;
+                  line-height: 3;
+                  display: flex;
+                  justify-content: flex-end;
+                  align-items: center;
+                `}
+              >
+                {issue.updatedAt}
+              </span>
+              <div
+                css={css`
+                  max-width: 100%;
+                  overflow-x: auto;
+                  overflow-wrap: anywhere;
+                  text-align: left;
                   word-wrap: break-word;
                   word-break: break-all;
-                  overflow-wrap: anywhere;
-                  line-break: anywhere;
                   white-space: pre-wrap;
-                }
-                a {
-                  text-decoration: underline;
-                }
-                code,
-                pre {
-                  background-color: var(--color-code-bg);
-                }
-                pre {
-                  padding: 0.4em 1em;
-                }
-              `}
-              dangerouslySetInnerHTML={{ __html: issue.bodyHTML }}
-            />
-          </li>
-        ))}
+                  & * {
+                    word-wrap: break-word;
+                    word-break: break-all;
+                    overflow-wrap: anywhere;
+                    line-break: anywhere;
+                    white-space: pre-wrap;
+                  }
+                  a {
+                    text-decoration: underline;
+                  }
+                  code,
+                  pre {
+                    background-color: var(--color-code-bg);
+                  }
+                  pre {
+                    padding: 0.4em 1em;
+                  }
+                `}
+                dangerouslySetInnerHTML={{ __html: issue.bodyHTML }}
+              />
+            </li>
+          ))}
+        </Masonry>
       </ul>
     </Layout>
   )
