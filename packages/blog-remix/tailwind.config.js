c7a65ec13f73933d2985093a7ba84d6ca015cdbc
Sehyun Chung <sehyun.chung@icloud.com>
2021-05-15 17:33:11 +0900

    feat: update remix (#156)

--- packages/blog-remix/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  purge: ["./app/**/*.{js,ts,tsx,md,mdx}", "./remix.config.js"],
-  darkMode: "media", // or 'media' or 'class'
-  theme: {},
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
