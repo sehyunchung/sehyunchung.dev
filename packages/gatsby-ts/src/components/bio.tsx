f9a9c8d6970cbe1d48322a30ec483e550fd6c428
Sehyun Chung <sehyun.chung@icloud.com>
2021-09-17 15:20:07 +0900

    refactor: migrate `Bio`

--- packages/gatsby-ts/src/components/bio.tsx
+++ packages/gatsby-ts/src/components/bio.tsx
@@ -1,7 +1,6 @@
-import React, { ComponentProps, FC } from 'react';
-import { graphql, Link, useStaticQuery } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby'
 
-import { styled } from '../stitches.config';
+import { styled } from '../stitches.config'
 
 const Nav = styled('nav', {
   display: 'inline-flex',
@@ -10,9 +9,9 @@ const Nav = styled('nav', {
     paddingLeft: '0.5em',
     paddingRight: '0.5em',
   },
-});
+})
 
-export const Bio: FC<ComponentProps<typeof Nav>> = (props) => {
+export default function Bio() {
   const data = useStaticQuery(graphql`
     query BioQuery {
       site {
@@ -25,17 +24,15 @@ export const Bio: FC<ComponentProps<typeof Nav>> = (props) => {
         }
       }
     }
-  `);
+  `)
 
-  const { social } = data.site.siteMetadata;
+  const { social } = data.site.siteMetadata
 
   return (
-    <Nav {...props}>
+    <Nav>
       <Link to="/about">about</Link>
       <Link to="/til">til</Link>
       <a href={`https://github.com/${social.github}`}>github</a>
     </Nav>
-  );
-};
-
-export default Bio;
+  )
+}
