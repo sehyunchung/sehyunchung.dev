782dc051ee3b2cb3ad6c2fd2120474857fc538ab
Sehyun Chung <sehyun.chung@icloud.com>
2022-06-30 20:44:03 +0900

    Change index page text

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,16 +4,8 @@ import { tw } from "@twind";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-    </div>
+    <h1 class={tw`p-4 mx-auto max-w-screen-md`}>
+      Welcome to `sehyunchung.dev` built with Deno/Fresh.
+    </h1>
   );
 }
