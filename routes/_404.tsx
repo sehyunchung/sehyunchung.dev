57e139dd4fcb6eeb37b528e7147a1ef3e76664a3
Sehyun Chung <sehyun.chung@icloud.com>
2022-07-02 00:29:20 +0900

    feat: add `Head` to pages

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,15 +1,22 @@
 /** @jsx h */
-import { h } from "preact";
+/** @jsxFrag Fragment */
+import { h, Fragment } from "preact";
 import { UnknownPageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
-    <body
-      class={tw`p-4 font-sans bg-white text-black dark:(bg-gray-800 text-white)`}
-    >
-      Hi, <code class={tw`text-gray-400`}>{url.pathname}</code> page is not
-      exist(yet).
-    </body>
+    <>
+      <Head>
+        <title>{url.pathname}</title>
+      </Head>
+      <body
+        class={tw`p-4 font-sans bg-white text-black dark:(bg-gray-800 text-white)`}
+      >
+        Hi, <code class={tw`text-gray-400`}>{url.pathname}</code> page is not
+        exist(yet).
+      </body>
+    </>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,12 @@
 /** @jsx h */
-import { h } from "preact";
+/** @jsxFrag Fragment */
+import { h, Fragment } from "preact";
 import { tw } from "@twind";
+import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
+
+const TITLE = `"Home Page"`;
+const DESCRIPTION = `"Home Page"`;
 
 const ME = [
   "the father of the most 😍 dog in the universe",
@@ -8,22 +14,32 @@ const ME = [
   "a DJ",
 ];
 
-export default function Home() {
+export default function Home(props: PageProps) {
   return (
-    <body
-      class={tw`p-4 font-sans bg-white text-black dark:(bg-gray-800 text-white)`}
-    >
-      <h1 class={tw`max-w-screen-md`}>Hi, my name is in url. And I'm...</h1>
-      <div class={tw`h-8`} />
-      <main>
-        <ul class={tw`flex flex-col gap-2`}>
-          {ME.map((item, index) => (
-            <li class={tw`flex items-center gap-1`} key={index}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      </main>
-    </body>
+    <>
+      <Head>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={props.url.href} />
+      </Head>
+      <body
+        class={tw`p-4 font-sans bg-white text-black dark:(bg-gray-800 text-white)`}
+      >
+        <h1 class={tw`max-w-screen-md`}>Hi, my name is in url. And I'm...</h1>
+        <div class={tw`h-8`} />
+        <main>
+          <ul class={tw`flex flex-col gap-2`}>
+            {ME.map((item, index) => (
+              <li class={tw`flex items-center gap-1`} key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </main>
+      </body>
+    </>
   );
 }
