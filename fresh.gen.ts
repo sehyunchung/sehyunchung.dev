832cf6a9d3d8995ff066d17f79d04723a6450b55
Sehyun Chung <sehyun.chung@icloud.com>
2022-07-02 00:07:11 +0900

    Remove joke api routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,15 +3,13 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/index.tsx": $1,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
