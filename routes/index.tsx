fee2ebb58cf3099a5145299fa831f4973be2115d
Sehyun Chung <sehyun.chung@icloud.com>
2022-07-01 23:50:12 +0900

    Change index page styles

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,10 +10,11 @@ const ME = [
 
 export default function Home() {
   return (
-    <div class={tw`font-mono`}>
-      <h1 class={tw`p-4 max-w-screen-md`}>Hi, my name is in url. And I'm...</h1>
+    <div class={tw`p-4 font-sans`}>
+      <h1 class={tw`max-w-screen-md`}>Hi, my name is in url. And I'm...</h1>
+      <div class={tw`h-8`} />
       <main>
-        <ul class={tw`flex flex-col gap-2 p-4`}>
+        <ul class={tw`flex flex-col gap-2`}>
           {ME.map((item, index) => (
             <li class={tw`flex items-center gap-1`} key={index}>
               {item}
