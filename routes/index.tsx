94a5a6e185566cee2c9e09336df2055b8cac1944
Sehyun Chung <sehyun.chung@icloud.com>
2022-07-01 17:27:00 +0900

    Remove links

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,12 +3,9 @@ import { h } from "preact";
 import { tw } from "@twind";
 
 const ME = [
-  {
-    text: "the father of the most 😍 dog in the universe",
-    link: "https://instagram.com/chocho_paju",
-  },
-  { text: "a frontend developer", link: "https://github.com/sehyunchung" },
-  { text: "a DJ", link: "https://soundcloud.com/cong_vu" },
+  "the father of the most 😍 dog in the universe",
+  "a frontend developer",
+  "a DJ",
 ];
 
 export default function Home() {
@@ -19,7 +16,7 @@ export default function Home() {
         <ul class={tw`flex flex-col gap-2 p-4`}>
           {ME.map((item, index) => (
             <li class={tw`flex items-center gap-1`} key={index}>
-              {item.text}
+              {item}
             </li>
           ))}
         </ul>
