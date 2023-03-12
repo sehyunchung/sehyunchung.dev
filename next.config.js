ef52e6fba9293f22a42866d8470dd6923a1448bd
Sehyun Chung <sehyun.chung@icloud.com>
2023-03-12 15:54:15 +0900

    n13

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
-});
+}
 
-module.exports = withMDX({
-  reactStrictMode: true,
-  // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages (mdx) that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false
-    }
-    return config
-  },
-});
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="h-8" />
+      <h1 className="text-4xl">sehyunchung.dev</h1>
+      <div className="h-12" />
+      <article className="text-base">
+        <h2 className="text-xl">Hello, I&apos;m...</h2>
+        <div className="h-7" />
+        <ul className="flex flex-col gap-3 list-disc pl-4">
+          <li>
+            the father of the most 😍 dog in the universe{" "}
+            <a
+              className="text-blue-500 underline"
+              href="https://instagram.com/chocho_paju"
+              target="_blank"
+              rel="noreferrer"
+            >
+              [link]
+            </a>
+          </li>
+          <li>
+            a frontend developer{" "}
+            <a
+              className="text-blue-500 underline"
+              href="https://github.com/sehyunchung"
+              target="_blank"
+              rel="noreferrer"
+            >
+              [link]
+            </a>
+          </li>
+          <li>
+            a DJ/Producer/etc.{" "}
+            <a
+              className="text-blue-500 underline"
+              href="https://soundcloud.com/cong_vu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              [link]
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
+}
