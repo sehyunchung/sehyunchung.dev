e14a341ae47286870d1a776630300b9507954035
Sehyun Chung <sehyun.chung@icloud.com>
2023-03-12 16:04:13 +0900

    Config fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        sans: [
+          "Inter",
+          "Gothic A1",
+          "Noto Sans KR",
+          "Noto Sans",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
         mono: ["Jetbrains Mono", "Hack", "Menlo", "Consolas", "monospace"],
         serif: ["Garamond", "Times New Roman", "Eulyoo1945-Regular"],
       },
