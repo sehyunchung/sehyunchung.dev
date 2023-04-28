3e06aad4d6d2ba1bd7617769419d84b9169fa0de
Sehyun Chung <sehyun.chung@icloud.com>
2023-04-28 22:56:04 +0900

    sort post by created date

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-        </article>
-      ))}
+    <div className="prose dark:prose-invert flex-auto">
+      {allPosts
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        .map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug} className="no-underline">
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
     </div>
   );
 }
