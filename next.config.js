1532692c932efdf725a38a099254a92795d0ad2f
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-14 23:08:15 +0900

    remove `experimental` from `next.config.js`

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const { withContentlayer } = require("next-contentlayer")
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = withContentlayer(nextConfig)
