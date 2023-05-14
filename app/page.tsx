587c53c2d92b71974b0172a8b2d9ba7312210807
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-14 21:39:05 +0900

    add created date in post list

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,18 @@ export default function Home() {
         .map((post) => (
           <article key={post._id}>
             <Link href={post.slug} className="no-underline">
-              <h2>{post.title}</h2>
+              <h2 className="flex justify-between items-center">
+                {post.title}
+                {post.date && (
+                  <pre className="m-0 p-0 bg-transparent text-xs text-slate-700 dark:text-slate-200">
+                    {new Date(post.date).toLocaleDateString("ko")}
+                  </pre>
+                )}
+              </h2>
+              {post.description && (
+                <p className="font-normal">{post.description}</p>
+              )}
             </Link>
-            {post.description && <p>{post.description}</p>}
           </article>
         ))}
     </div>
