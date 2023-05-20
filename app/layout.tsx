251471b7f93bf7d092a7c46c318e37f0cf3552d6
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-20 14:36:06 +0900

    Blog -> Posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
-                  <Link href="/posts">Blog</Link>
+                  <Link href="/posts">Posts</Link>
                   <Link href="/til">TIL</Link>
                   <Link href="/about">About</Link>
                 </nav>
