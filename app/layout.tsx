a75a13f2472c00e780bd5bbea1352999a6d9ddc0
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-20 14:12:52 +0900

    use `/posts` for post list page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
-                  <Link href="/til">til</Link>
+                  <Link href="/posts">Blog</Link>
+                  <Link href="/til">TIL</Link>
                   <Link href="/about">About</Link>
                 </nav>
               </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,3 @@
-import Link from "next/link"
-import { allPosts } from "@/.contentlayer/generated"
-
 export default function Home() {
-  return (
-    <div className="prose dark:prose-invert flex-auto">
-      {allPosts
-        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-        .map((post) => (
-          <article key={post._id}>
-            <Link href={post.slug} className="no-underline">
-              <h2 className="flex flex-col justify-between">
-                <div className="m-0 p-0 mb-2 text-xs bg-transparent text-slate-700 dark:text-slate-200">
-                  {new Date(post.date).toLocaleDateString("ko")}
-                </div>
-                {post.title}
-              </h2>
-              {post.description && (
-                <p className="font-normal">{post.description}</p>
-              )}
-            </Link>
-          </article>
-        ))}
-    </div>
-  )
+  return <div className="prose dark:prose-invert flex-auto"></div>
 }
