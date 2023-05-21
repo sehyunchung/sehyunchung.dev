e6835506cd9e501667ad15a019645fb654141b17
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-21 19:00:30 +0900

    resolve route type error

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,4 @@
+import type { Route } from "next"
 import Link from "next/link"
 import { allPosts } from "@/.contentlayer/generated"
 
@@ -8,7 +9,7 @@ export default async function PostsPage() {
         .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
         .map((post) => (
           <article key={post._id}>
-            <Link href={`/${post.slug}`} className="no-underline">
+            <Link href={`/${post.slug}` as Route} className="no-underline">
               <h2 className="mb-2 flex flex-col justify-between">
                 <div className="m-0 mb-2 bg-transparent p-0 text-xs text-slate-700 dark:text-slate-200">
                   {new Date(post.date).toLocaleDateString("ko")}
