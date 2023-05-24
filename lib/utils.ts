befe27d6c1ff3b79cfa5202451ec5b608495b8a8
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-24 13:04:32 +0900

    hey me... lmao... wtf... why...

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,10 +14,10 @@ export function cn(...inputs: ClassValue[]) {
 export function getOgImgUrl(): URL {
   const baseUrl =
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
+      ? "http://localhost:3000/api/og"
       : process.env.VERCEL_URL
-      ? "https://" + process.env.VERCEL_URL
-      : "https://sehyunchung.dev"
+      ? `https://${process.env.VERCEL_URL}/api/og`
+      : "https://sehyunchung.dev/api/og"
 
   return new URL(baseUrl)
 }
