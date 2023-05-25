6d61a05c4331c72a8972419f751c8b8990479b64
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-26 02:20:00 +0900

    fix typo............

--- app/til/layout.tsx
+++ app/til/layout.tsx
@@ -4,7 +4,7 @@ import { TilPageAlert } from "@/components/til-alert"
 
 export default function TILLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="prose dark:proxe-invert pt-8">
+    <div className="prose dark:prose-invert pt-8">
       <h1 className="uppercase">
         <Link className="font-bold no-underline" href="/til">
           til
