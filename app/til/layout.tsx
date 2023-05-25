0948a6619296b60936d754f0fd8ffe8f9c6600c8
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-25 15:40:55 +0900

    add link to 'TIL' heading

--- app/til/layout.tsx
+++ app/til/layout.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link"
+
 import { TilPageAlert } from "@/components/til-alert"
 
 export default function TILLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="pt-8 prose dark:prose-invert prose-img:rounded-xl prose-img:m-0">
-      <h1 className="uppercase">til</h1>
+    <div className="prose dark:proxe-invert pt-8">
+      <h1 className="uppercase">
+        <Link className="font-bold no-underline" href="/til">
+          til
+        </Link>
+      </h1>
       <TilPageAlert />
       {children}
     </div>
