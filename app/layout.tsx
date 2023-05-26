dd7842751275929c0ce20f789da5732bcedbbf46
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-26 18:56:47 +0900

    add ms clarity (#194)
    * add ms clarity script (using worker)
    
    * let's just show imperfection (space between box drawing chars)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 
 import "./globals.css"
+import Script from "next/script"
+
 import { comicMono, comicNeue, gothicA1 } from "@/lib/fonts"
 import { Analytics } from "@/components/analytics"
 import { LogoNav } from "@/components/logo-nav"
@@ -65,6 +67,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Analytics />
         </ThemeProvider>
       </body>
+      <Script id="ms-clarity" strategy="worker">{`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "h9xndue0yj");
+      `}</Script>
     </html>
   )
 }
