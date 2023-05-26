c4bac7d7a16ac3e686404c4d579408a61456891e
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-26 17:38:29 +0900

    use text length for width

--- components/ascii.tsx
+++ components/ascii.tsx
@@ -1,58 +1,57 @@
 const Box = ({
-  width,
-  height = 3,
   text,
-}: {
-  width: number
-  height?: number
-  text: string
-}) => {
+  width,
+  ...props
+}: { text: string; width?: number } & React.ComponentProps<"pre">) => {
+  const height = 3
+  const w = width ? width : text.length + 2
+
   const dimension = Array.from({ length: height }, () =>
-    Array.from({ length: width }, () => " ")
+    Array.from({ length: w }, () => " ")
   )
   const top = dimension[0].map((_, i) => {
     if (i === 0) return "┌"
-    if (i === width - 1) return "┐"
+    if (i === w - 1) return "┐"
     return "─"
   })
   const bottome = dimension[2].map((_, i) => {
     if (i === 0) return "└"
-    if (i === width - 1) return "┘"
+    if (i === w - 1) return "┘"
     return "─"
   })
   const middle = dimension[1].map((_, i) => {
     if (i === 0) return "│"
-    if (i === width - 1) return "│"
+    if (i === w - 1) return "│"
     return " "
   })
 
   return (
-    <div className="relative flex flex-col text-xs ">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <pre role="none" className="relative flex flex-col text-xs " {...props}>
+      <pre className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         {text}
-      </div>
-      <div className="flex">
+      </pre>
+      <pre className="flex">
         {top.map((t, i) => (
-          <code className="flex-1 flex" key={i}>
+          <code className="flex-1 flex justify-center" key={i}>
             {t}
           </code>
         ))}
-      </div>
-      <div className="flex translate-x-[1px]">
+      </pre>
+      <pre className="flex">
         {middle.map((t, i) => (
           <code className="flex-1 flex justify-center" key={i}>
             {t}
           </code>
         ))}
-      </div>
-      <div className="flex">
+      </pre>
+      <pre className="flex">
         {bottome.map((t, i) => (
-          <code className="flex-1 flex" key={i}>
+          <code className="flex-1 flex justify-center" key={i}>
             {t}
           </code>
         ))}
-      </div>
-    </div>
+      </pre>
+    </pre>
   )
 }
 
