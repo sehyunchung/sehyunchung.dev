b58f0ad95e347953357326bc402a179d125fca86
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-26 20:39:37 +0900

    lift prose up to root layout

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata() {
 
 export default async function PostsPage() {
   return (
-    <div className="prose dark:prose-invert flex-auto">
+    <div className="flex-auto">
       {allPosts
         .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
         .map((post) => (
