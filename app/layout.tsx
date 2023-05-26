a2af8f7912e104a73b53fb1860f047daee65b6ff
Sehyun Chung <sehyun.chung@icloud.com>
2023-05-26 22:21:38 +0900

    beforeInteractive?

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       suppressHydrationWarning
     >
       <head>
-        <Script id="ms-clarity">{`
+        <Script id="ms-clarity" strategy="beforeInteractive">{`
       (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
