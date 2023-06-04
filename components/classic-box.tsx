34490141b113b6b1f1e11ac1884d699612cd269c
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-05 01:06:09 +0900

    add `color` prop

--- components/classic-box.tsx
+++ components/classic-box.tsx
@@ -3,22 +3,28 @@ import { cn } from "@/lib/utils"
 import { stringDigger } from "./box-drawn"
 
 export function ClassicBox({
+  color = "#d2d2ff",
   children,
   className,
-}: React.ComponentProps<"div"> & { children: React.ReactNode }) {
+}: React.ComponentProps<"div"> & {
+  children: React.ReactNode
+  color?: React.CSSProperties["backgroundColor"]
+}) {
   const initial = stringDigger(children)[0]
 
   return (
     <div
       style={{
         borderStyle: "outset",
+        borderColor: color,
+        backgroundColor: color,
       }}
       className={cn(
-        "relative flex justify-between border-4 border-[#d2d2ff] bg-[#d2d2ff] h-[2em] px-[1em] pl-[1.5em] items-center leading-none font-bold italic capitalize font-serif",
+        "relative flex justify-between border-4 h-[2em] px-[1em] pl-[1.5em] items-center leading-none font-bold italic capitalize font-serif",
         className
       )}
     >
-      <span className="absolute drop-shadow-md left-[0.2em] bottom-[0.05em] text-[150%] flex font-gothic not-italic text-[#ff0] font-black">
+      <span className="absolute drop-shadow-md left-[0.2em] top-[2%] text-[150%] flex font-gothic not-italic text-[#ff0] font-black">
         {initial}
       </span>
       <span className="flex">{children}</span>
