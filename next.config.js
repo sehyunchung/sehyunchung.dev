a5372299d321ac044aedfc3b337609fe3835a740
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-06 09:02:14 +0900

    remove unused

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const { withContentlayer } = require("next-contentlayer")
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: ["shiki", "vscode-oniguruma"],
-    outputFileTracingIncludes: {
-      shiki: "node_modules/.pnpm/**/shiki/**/*.json",
-    },
-  },
   images: {
     domains: ["user-images.githubusercontent.com", "github.com"],
     formats: ["image/avif", "image/webp"],
