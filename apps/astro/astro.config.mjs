383e4c113ce9a22a80ed31bca847fccfc0e26c5c
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-09 23:41:27 +0900

    add bunch of stuff

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), image(), mdx(), prefetch(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
