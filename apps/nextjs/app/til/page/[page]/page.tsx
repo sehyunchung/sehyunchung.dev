d5e4052267d3ae6d2fc1f38ba13c135906daf514
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-12 00:33:52 +0900

    use arrow for pagination

--- apps/nextjs/app/til/page/[page]/page.tsx
+++ apps/nextjs/app/til/page/[page]/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react"
 import { allTils } from "@/.contentlayer/generated/"
 
 import { cn } from "@/lib/utils"
@@ -6,61 +7,67 @@ import { cn } from "@/lib/utils"
 import { TILItem } from "../../components"
 
 const sortedTils = allTils.sort(
-  (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+	(a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
 )
 
 const getPaginatedTils = (page: string) => {
-  const p = Number(page) - 1
-  return sortedTils.slice(p * 10, p * 10 + 10)
+	const p = Number(page) - 1
+	return sortedTils.slice(p * 10, p * 10 + 10)
 }
 
 function chunkArray<T>(array: T[], chunk: number): T[][] {
-  const result = []
-  for (let i = 0; i < array.length; i += chunk) {
-    result.push(array.slice(i, i + chunk))
-  }
-  return result
+	const result = []
+	for (let i = 0; i < array.length; i += chunk) {
+		result.push(array.slice(i, i + chunk))
+	}
+	return result
 }
 
 export async function generateStaticParams() {
-  return chunkArray(allTils, 10).map((chunk, index) => index + 1)
+	return chunkArray(allTils, 10).map((chunk, index) => index + 1)
 }
 
 export default function TilPaginatedPage({
-  params: { page },
+	params: { page },
 }: {
-  params: { page: string }
+	params: { page: string }
 }) {
-  const currentPageTils = getPaginatedTils(page)
-  const totalPages = Math.ceil(allTils.length / 10)
+	const currentPageTils = getPaginatedTils(page)
+	const totalPages = Math.ceil(allTils.length / 10)
+	const currentPage = Number(page)
+	const prevPage = currentPage > 1 ? currentPage - 1 : 1
+	const nextPage = currentPage < 10 ? currentPage + 1 : 10
 
-  return (
-    <>
-      <article className="w-[100%]">
-        {currentPageTils
-          .sort(
-            (a, b) =>
-              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-          )
-          .map((til) => (
-            <TILItem
-              key={til.id}
-              className="break-words border-b border-b-gray-200 pb-6"
-              til={til}
-            />
-          ))}
-      </article>
-      <div className="flex justify-center mt-8">
-        {Array.from({ length: totalPages }, (_, i) => (
-          <Link
-            key={`page-${i + 1}`}
-            href={`/til/page/${i + 1}`}
-            className={cn("px-4 py-2 mx-2 rounded-md", i + 1 === Number(page) ? 'underline' : 'no-underline')}
-          >
-            {i + 1}
-          </Link>
-        ))}
-      </div>
-    </>
-  )
+	return (
+		<>
+			<article className="w-[100%]">
+				{currentPageTils
+					.sort(
+						(a, b) =>
+							new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+					)
+					.map((til) => (
+						<TILItem
+							key={til.id}
+							className="break-words border-b border-b-gray-200 pb-6"
+							til={til}
+						/>
+					))}
+			</article>
+			<div className="flex justify-center items-center mt-8 gap-10">
+				<Link
+					className={cn(currentPage === 1 ? "hidden" : "")}
+					href={`/til/page/${prevPage}`}
+				>
+					<ArrowLeftIcon className="w-10 h-10" />
+				</Link>
+				<Link
+					className={cn(currentPage === totalPages ? "hidden" : "")}
+					href={`/til/page/${nextPage}`}
+				>
+					<ArrowRightIcon className="w-10 h-10" />
+				</Link>
+			</div>
+		</>
+	)
 }
